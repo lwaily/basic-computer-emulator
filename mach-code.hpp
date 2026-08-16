652e9da596798a77241c7f2999cf85cc7c663dc3
Ali A <124992856+lwaily@users.noreply.github.com>
2026-08-15 20:05:18 -0700

    Update mach-code.hpp

--- mach-code.hpp
+++ mach-code.hpp
@@ -1,6 +1,6 @@
 /**
  * Name:  Ali Alwaily
- * Date:  4 July 2026
+ * Date:  15 August 2026
  * About: This file sets up the structure of the cpu class, with an inner class
  * 			called the reg class that takes care of initializing all 16
  * 			registers.
@@ -20,152 +20,72 @@
 #ifndef MC_H
 #define MC_H
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <bitset>
+#include <vector>	// loads vector data structure
 
-#define MAX_REG		16
+#define SIZE_KB 32000
 
-/**
- * TODO CREATE 4 KB of MEMORY to ACT as THE COMPUTER MEMORY as if you're
- * 	working with a 4 KB COMPUTER.
- */
+int machine_code_emulator();
 
 /**
- * CPU Class
+ *
  */
-class cpu {
-	public:
+class program {
+	private:
 		/**
-		 * Register Class
-		 *		Registers here are represented as integer data types, which are 32-bits
-		 *		 long, or 4 bytes. I have mostly followed ARM architure in the way I set
-		 *		 up registers. Every register is accessed through the reg_arr[] array.
+		 *
 		 */
-		class reg {
+		char max_program_size[SIZE_KB];
+	public:
+		//setter for max_program_size
+		//getter for max_program_size
+
+
+		/**
+		 * Instructions Class
+		 *		This class is used to parse through every 32-bit input and break it up
+		 *		into 6 categories:
+		 *			Condition:			[See Enum Defined Below]
+		 *			Operation Code:		[See Enum Defined Below]
+		 *			Immediate Bit:		For any instruction other than branching:
+		 * 								If 0, operand 2 will be treated as a register,
+		 *								If 1, operand 2 will be treated as an immediate.
+		 *								For Branching,
+		 *								If 0, branch will not link,
+		 *								If 1, this will represent branching and linking.
+		 *			Destination Reg:	//
+		 *			Oper1:				//
+		 *			Oper2:				//
+		 */
+		class instr {
 			public:
 				/**
-				* Structure of the Register array reg_arr[]
-				*/
-				int reg_arr[MAX_REG];
-				/**
-				* Constructor that initializes the registers.
-				*/
-				reg(){
-					/**
-					* ----------  SCHEME  -------------------------------------
-					* REG_0  = General Purpose, Argument, Return Value
-					* 											[Not Preserved]
-					* REG_1  = General Purpose, Argument 		[Not Preserved]
-					* REG_2  = General Purpose, Argument 		[Not Preserved]
-					* REG_3  = General Purpose, Argument 		[Not Preserved]
-					* REG_4  = General Purpose					[Preserved]
-					* REG_5  = General Purpose 					[Preserved]
-					* REG_6  = General Purpose 					[Preserved]
-					* REG_7  = General Purpose 					[Preserved]
-					* REG_8  = General Purpose 					[Preserved]
-					* REG_9  = General Purpose 					[Preserved]
-					* REG_10 = General Purpose 					[Preserved]
-					* REG_11 = Frame Pointer 					[Preserved]
-					* REG_12 = Instra Procedure Call 			[Not Preserved]
-					* REG_13 = Stack Pointer  					[Preserved]
-					* REG_14 = Link Register 					[Preserved]
-					* REG_15 = Program Counter 					[Not Preserved]
-					*/
-					for(int i = 0; i < 11; ++i) { reg_arr[i] = 0; }
-					reg_arr[11] = 0;
-					reg_arr[12] = 0;
-					reg_arr[13] = 0;
-					reg_arr[14] = 0;
-					reg_arr[15] = 4;
-				}
-				/**
-				 *
+				 * 0b_COND_OPCD_I001_DEST_OPR1_OPER_2REG_IMME
 				 */
-				void incr_pc();
-		};	// end of reg class
-		/**
-		 * aka: Current Program Status Register
-		 */
-		double CPSR;
-		//
-		/**
-		 * Constructor that initializes the CPU
-		 */
-		cpu(){
-			/**
-			 * ----------  STRUCTURE  OF  THE  CPSR  --------------------------
-			 * 	Based off of how it works in ARM.
-			 * ``Conditions which are used to construct conditional loops and
-			 * 		other branching are based on condition flags, sometimes
-			 * 		referred to as the ALU flags stored in PSTATE, which are in
-			 * 		the global condition flag register, NZCV (shown in my ARM64
-			 * 		Register Architecture diagram)."
-			 * 	Reference:
-			 * https://eclecticlight.co/2021/06/23/code-in-arm-assembly-controlling-flow/
-			 * ----------------------------------
-			 * The CPSR variable is 8 bytes, which is perfect for what I what I
-			 * 	want to do, which is to have 4 bits of room for the flags
-			 * 	outlined below AND allow up to 16 nested if-statements and for-
-			 * 	loops.
-			 * I really don't get how the CPU really does this, but this is
-			 * 	the best I've come up with to simulate the behaivor.
-			 * ----------  FLAGS  ---------------------------------------------
-			 * N - Negative condition code flag;
-			 * 			1: negative; 0 otherwise
-			 * Z - Zero condition code flag
-			 *			1: result is 0; 0 otherwise
-			 * C - Carry condition code flag
-			 * 			1: carry produced; 0 otherwise
-			 * V - oVerflow condition code flag
-			 * 			1: overflow produced; 0 otherwise
-			 */
-			CPSR = 0;
-		}
-};	// end of cpu class
+				unsigned int condition;			// COND
+				unsigned int opcode;			// OPCD
+				unsigned int immediate_bit;		// I
+				unsigned int dest_reg;			// DEST
+				unsigned int oper1_reg;			// OPR1
+				unsigned int oper2;				// OPER_2REG_IMME
 
-/**
- * Instructions Class
- *		This class is used to parse through every 32-bit input and break it up 
- *		into 6 categories:
- *			Condition:			[See Enum Defined Below]
- *			Operation Code:		[See Enum Defined Below]
- *			Immediate Bit:		For any instruction other than branching:
- * 								If 0, operand 2 will be treated as a register,
- *								If 1, operand 2 will be treated as an immediate.
- *								For Branching,
- *								If 0, branch will not link,
- *								If 1, this will represent branching and linking.
- *			Destination Reg:	//
- *			Oper1:				//
- *			Oper2:				//
- */
-class instr {
-	public:
+				/**
+				 * Constructor
+				 * @param value
+				 */
+				instr(unsigned int value){
+					condition		= (value & 0xf0000000) >> 28;
+					opcode			= (value & 0x0f000000) >> 24;
+					immediate_bit	= (value & 0x00800000) >> 23;
+					dest_reg		= (value & 0x000f0000) >> 16;
+					oper1_reg		= (value & 0x0000f000) >> 12;
+					oper2			= (value & 0x00000fff);
+				}
+		};	// end of instr class
 		/**
-		 * 0b_COND_OPCD_I001_DEST_OPR1_OPER_2REG_IMME
+		 * A global vector that holds the instructions for a program.
 		 */
-		unsigned int condition;			// COND
-		unsigned int opcode;			// OPCD
-		unsigned int immediate_bit;		// I
-		unsigned int dest_reg;			// DEST
-		unsigned int oper1_reg;			// OPR1
-		unsigned int oper2;				// OPER_2REG_IMME
-		
-		/**
-		 * Constructor
-		 * @param value
-		 */
-		instr(unsigned int value){
-			condition		= (value & 0xf0000000) >> 28;
-			opcode			= (value & 0x0f000000) >> 24;
-			immediate_bit	= (value & 0x00800000) >> 23;
-			dest_reg		= (value & 0x000f0000) >> 16;
-			oper1_reg		= (value & 0x0000f000) >> 12;
-			oper2			= (value & 0x00000fff);
-		}
-};	// end of instr class
+		std::vector<instr> program_instructions;
+};
 
 /**
  * Condition Enum that represents the value of every condition
@@ -180,7 +100,7 @@ enum cond {
 	AL = 14,	// ALWAYS
 };
 
-/** 
+/**
  * Op_code Enum that represents the value of every operation code
  */
 enum op_code {
@@ -206,9 +126,4 @@ enum op_code {
 	B	= 15,	//
 };
 
-/**
- * A global vector that holds the instructions for a program.
- */
-std::vector<instr> program;
-
-#endif 
+#endif
