b101aadaaed5adaca1cd330acff7056dfcf2666f
Ali A <124992856+lwaily@users.noreply.github.com>
2026-08-15 20:05:50 -0700

    Update mach-code.cpp

--- mach-code.cpp
+++ mach-code.cpp
@@ -1,6 +1,6 @@
 /**
  * Name:  Ali Alwaily
- * Date:  4 July 2026
+ * Date:  15 August 2026
  * About: This file acts as the ``entry point" into the program. It defines the
  * 			incr_pr() function, initializes variables, asks for user input and
  * 			processes that, then begins "executing" those instructions.
@@ -8,19 +8,19 @@
  * 				up any dynamically-allocated memory.
  */
 
-#include "mach-code.hpp"
+#include "comp-emu.hpp"		//
+#include "mach-code.hpp"	//
 
-/**
- * The Increment-PC function.
- */
-void cpu::reg::incr_pc(){
-	cpu::reg::reg_arr[15] += 4;
-}
+#include <iostream>			//
+#include <string>			//
+#include <bitset>			//
 
 /**
  * Main Function
  */
-int main(){
+int machine_code_emulator(){
+	program new_program;
+
 	// Local Variables
 	std::string  input_str;
 	unsigned int inst = 4294967295;
@@ -37,8 +37,8 @@ int main(){
 			std::bitset<32> bits(input_str);
 			inst = static_cast<unsigned int>(bits.to_ulong());
 			// Create new instr object and push pack into program[] vector
-			instr my_instr(inst);
-			program.push_back(my_instr);
+			program::instr my_instr(inst);
+			new_program.program_instructions.push_back(my_instr);
 		} else {
 			/**
 			 * Currently, there will be no additional checks to see if the
@@ -49,67 +49,82 @@ int main(){
 	}
 
 	// Creates a reg object
-	cpu::reg my_reg;
+	COMP::cpu::reg my_reg;
 
+/*	
+	// TEST
+	std::cout << "----  TEST  ----" << std::endl;
+	for(unsigned int i = 0; i < program.size(); ++i){
+		std::cout <<
+				program[i].condition		<< " " <<
+				program[i].opcode			<< " " <<
+				program[i].immediate_bit	<< " " <<
+				program[i].dest_reg			<< " " <<
+				program[i].oper1_reg		<< " " <<
+				program[i].oper2			<< " " 
+				<< std::endl;
+	}
+	std::cout << "----------------" << std::endl;
+*/
 	// Parse the instructions and ``execute" them
-	unsigned int n = program.size()
+	unsigned int n = new_program.program_instructions.size();
 	for(unsigned int i = 0; i < n; ++i){
 		/**
 		 *
 		 */
-		if(program[i].opcode == AND){
-			if(program[i].immediate_bit == 0){
-				int rd = (int)program[i].dest_reg;
-				int rn = (int)program[i].oper1_reg;
-				int op = (int)program[i].oper2;
+		if(new_program.program_instructions[i].opcode == AND){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int rn = (int)new_program.program_instructions[i].oper1_reg;
+				int op = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] & my_reg.reg_arr[op];
-			} else if(program[i].immediate_bit == 1){
-				int rd  = (int)program[i].dest_reg;
-				int rn  = (int)program[i].oper1_reg;
-				int imm = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd  = (int)new_program.program_instructions[i].dest_reg;
+				int rn  = (int)new_program.program_instructions[i].oper1_reg;
+				int imm = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] & imm;
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == ORR){
-			if(program[i].immediate_bit == 0){
-				int rd = (int)program[i].dest_reg;
-				int rn = (int)program[i].oper1_reg;
-				int op = (int)program[i].oper2;
+		else if(new_program.program_instructions[i].opcode == ORR){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int rn = (int)new_program.program_instructions[i].oper1_reg;
+				int op = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] | my_reg.reg_arr[op];
-			} else if(program[i].immediate_bit == 1){
-				int rd  = (int)program[i].dest_reg;
-				int rn  = (int)program[i].oper1_reg;
-				int imm = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd  = (int)new_program.program_instructions[i].dest_reg;
+				int rn  = (int)new_program.program_instructions[i].oper1_reg;
+				int imm = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] | imm;
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == XOR){
-			if(program[i].immediate_bit == 0){
-				int rd = (int)program[i].dest_reg;
-				int rn = (int)program[i].oper1_reg;
-				int op = (int)program[i].oper2;
+		else if(new_program.program_instructions[i].opcode == XOR){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int rn = (int)new_program.program_instructions[i].oper1_reg;
+				int op = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] ^ my_reg.reg_arr[op];
-			} else if(program[i].immediate_bit == 1){
-				int rd  = (int)program[i].dest_reg;
-				int rn  = (int)program[i].oper1_reg;
-				int imm = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd  = (int)new_program.program_instructions[i].dest_reg;
+				int rn  = (int)new_program.program_instructions[i].oper1_reg;
+				int imm = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] ^ imm;
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == BIC){
-			if(program[i].immediate_bit == 0){
-				int rd = (int)program[i].dest_reg;
-				int rn  = (int)program[i].oper1_reg;
-				int op = (int)program[i].oper2;
+		else if(new_program.program_instructions[i].opcode == BIC){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int rn  = (int)new_program.program_instructions[i].oper1_reg;
+				int op = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] & ~(my_reg.reg_arr[op]);
 
 				std::cout << rd << std::endl
@@ -119,107 +134,107 @@ int main(){
 						<< my_reg.reg_arr[rn] << std::endl
 						<< ~(my_reg.reg_arr[op]) << std::endl;
 
-			} else if(program[i].immediate_bit == 1){
-				int rd  = (int)program[i].dest_reg;
-				int rn  = (int)program[i].oper1_reg;
-				int imm = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd  = (int)new_program.program_instructions[i].dest_reg;
+				int rn  = (int)new_program.program_instructions[i].oper1_reg;
+				int imm = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] & ~(imm);
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == LSR){
+		else if(new_program.program_instructions[i].opcode == LSR){
 			//
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == ASR){
+		else if(new_program.program_instructions[i].opcode == ASR){
 			//
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == LSL){
+		else if(new_program.program_instructions[i].opcode == LSL){
 			//
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == ADD){
-			if(program[i].immediate_bit == 0){
-				int rd = (int)program[i].dest_reg;
-				int rn = (int)program[i].oper1_reg;
-				int op = (int)program[i].oper2;
+		else if(new_program.program_instructions[i].opcode == ADD){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int rn = (int)new_program.program_instructions[i].oper1_reg;
+				int op = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] + my_reg.reg_arr[op];
-			} else if(program[i].immediate_bit == 1){
-				int rd  = (int)program[i].dest_reg;
-				int rn  = (int)program[i].oper1_reg;
-				int imm = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd  = (int)new_program.program_instructions[i].dest_reg;
+				int rn  = (int)new_program.program_instructions[i].oper1_reg;
+				int imm = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] + imm;
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == SUB){
-			if(program[i].immediate_bit == 0){
-				int rd = (int)program[i].dest_reg;
-				int rn = (int)program[i].oper1_reg;
-				int op = (int)program[i].oper2;
+		else if(new_program.program_instructions[i].opcode == SUB){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int rn = (int)new_program.program_instructions[i].oper1_reg;
+				int op = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] - my_reg.reg_arr[op];
-			} else if(program[i].immediate_bit == 1){
-				int rd = (int)program[i].dest_reg;
-				int rn  = (int)program[i].oper1_reg;
-				int imm = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int rn  = (int)new_program.program_instructions[i].oper1_reg;
+				int imm = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[rn] - imm;
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == MOV){
-			if(program[i].immediate_bit == 0){
-				int rd = (int)program[i].dest_reg;
-				int value = (int)program[i].oper2;
+		else if(new_program.program_instructions[i].opcode == MOV){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int value = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = my_reg.reg_arr[value];
-			} else if(program[i].immediate_bit == 1){
-				int rd = (int)program[i].dest_reg;
-				int value = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd = (int)new_program.program_instructions[i].dest_reg;
+				int value = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = value;
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == MVN){
-			if(program[i].immediate_bit == 0){
-				int rd      = (int)program[i].dest_reg;
-				int reg_val = (int)program[i].oper2;
+		else if(new_program.program_instructions[i].opcode == MVN){
+			if(new_program.program_instructions[i].immediate_bit == 0){
+				int rd      = (int)new_program.program_instructions[i].dest_reg;
+				int reg_val = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = ~(my_reg.reg_arr[reg_val]) + 1;
-			} else if(program[i].immediate_bit == 1){
-				int rd    = (int)program[i].dest_reg;
-				int value = (int)program[i].oper2;
+			} else if(new_program.program_instructions[i].immediate_bit == 1){
+				int rd    = (int)new_program.program_instructions[i].dest_reg;
+				int value = (int)new_program.program_instructions[i].oper2;
 				my_reg.reg_arr[rd] = ~(value) + 1;
 			}
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == LDR){
+		else if(new_program.program_instructions[i].opcode == LDR){
 			//
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == STR){
+		else if(new_program.program_instructions[i].opcode == STR){
 			//
 		}
 		/**
 		 *
 		 */
-		else if(program[i].opcode == CMP){
+		else if(new_program.program_instructions[i].opcode == CMP){
 			/**
 			 * cmp a, b
 			 * \where a is rd and b is oper2
@@ -238,7 +253,7 @@ int main(){
 		/**
 		 *
 		 */
-		else if(program[i].opcode == B){
+		else if(new_program.program_instructions[i].opcode == B){
 			//
 		}
 		/**
@@ -263,4 +278,3 @@ int main(){
 
 	return 0;
 }
-
